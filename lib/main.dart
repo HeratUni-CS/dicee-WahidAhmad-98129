dcdd49dfdcb7fb1f9c3dd3fe7dfa9a34098b2c92
WahidAhmad-98129 <wahidHadayat.personal.info@gmail.com>
2023-05-27 17:27:38 +0430

    Last Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,67 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('Dice'),
-        ),
-        body: MyApp(),
-      ),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  void main(){
+    runApp(myApp());
   }
-}
+
+  class myApp extends StatefulWidget {
+    @override
+    State<myApp> createState() => _myAppState();
+  }
+  class _myAppState extends State<myApp> {
+    @override
+
+    int leftDice=4;
+    int rightDice=5;
+
+    void  changeDiceNumber(){
+      leftDice=Random().nextInt(6)+1;
+      rightDice=Random().nextInt(6)+1;
+    }
+
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home:Scaffold(
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Center(
+              child: Text(
+                  'Dicee Appication',
+                  style: TextStyle(
+                fontSize: 35,
+              ),
+              ),
+            ),
+          ),
+          body: Center(
+            child: Container(
+              child: Row(
+                children: [
+                 Expanded(child: TextButton(
+                   onPressed: (){
+                     setState(() {
+                       changeDiceNumber();
+                     });
+                   },
+                   child: Image.asset('images/dice$leftDice.png'),
+                 ),
+                 ),
+
+                  Expanded(child: TextButton(
+                    onPressed: (){
+                      setState(() {
+                        changeDiceNumber();
+                      });
+                    },
+                    child: Image.asset('images/dice$rightDice.png'),
+                  ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+  }
